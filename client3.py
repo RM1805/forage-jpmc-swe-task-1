10e99cda1993b0cbf2ee2836f1947068dbab68dd
Rishi <rishimishra18052001@gmail.com>
2023-09-21 13:14:18 +0700

    new change

---
+++ client3.py
@@ -0,0 +1,77 @@
+################################################################################
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the "Software"),
+#  to deal in the Software without restriction, including without limitation
+#  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#  and/or sell copies of the Software, and to permit persons to whom the
+#  Software is furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in
+#  all copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#  DEALINGS IN THE SOFTWARE.
+
+import json
+import random
+import urllib.request
+
+# Server API URLs
+QUERY = "http://localhost:8080/query?id={}"
+
+# 500 server requests
+N = 500
+
+
+def getDataPoint(quote):
+    """ Produce all the needed values to generate a datapoint """
+    stock = quote['stock']
+    bid_price = float(quote['top_bid']['price'])
+    ask_price = float(quote['top_ask']['price'])
+    price = bid_price
+    return stock, bid_price, ask_price, price
+
+
+def getRatio(price_a, price_b):
+    """ Get ratio of price_a and price_b """
+    if price_b == 0:
+        # Avoid division by zero
+        return None
+    return price_a / price_b
+
+
+# Main
+if __name__ == "__main__":
+    # Query the price once every N seconds.
+    for _ in range(N):
+        quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
+
+        # Initialize variables to store prices for ratio calculation
+        price_a = None
+        price_b = None
+
+        for quote in quotes:
+            stock, bid_price, ask_price, price = getDataPoint(quote)
+            print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
+
+            # Store the first two prices for ratio calculation
+            if price_a is None:
+                price_a = price
+            elif price_b is None:
+                price_b = price
+            else:
+                # Once we have two prices, break out of the loop
+                break
+
+        if price_a is not None and price_b is not None:
+            ratio = getRatio(price_a, price_b)
+            if ratio is not None:
+                print("Ratio %s" % ratio)
+        else:
+            print("Not enough data to calculate ratio")
